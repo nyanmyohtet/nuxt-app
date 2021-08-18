74dd959c10d22ff2d6c15d11e21e6a8ff62c803a
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2021-08-18 22:55:58 +0630

    Setup GitHub Page Deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/nuxt-app/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-app',
